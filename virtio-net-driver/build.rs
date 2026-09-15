6f8bd6fd5581e620fbcc89296cf1f34f98467872
tas0dev <156896971+tas0dev@users.noreply.github.com>
2026-09-15 18:12:06 +0900

    Fix virtio net driver load address

--- virtio-net-driver/build.rs
+++ virtio-net-driver/build.rs
@@ -1,10 +1,8 @@
 fn main() {
     let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR missing");
-    if std::env::var("CARGO_CFG_TARGET_OS").as_deref() == Ok("mochios") {
-        println!(
-            "cargo:rustc-link-arg-bin=virtio-net-driver=-T{}/linker.ld",
-            manifest_dir
-        );
-    }
+    println!(
+        "cargo:rustc-link-arg-bin=virtio-net-driver=-T{}/linker.ld",
+        manifest_dir
+    );
     println!("cargo:rerun-if-changed={}/linker.ld", manifest_dir);
 }
